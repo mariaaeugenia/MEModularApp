2c6e04605c433b5f71d6c70561b88d3be2a78f66
Maria Eugênia <beds03@gmail.com>
2019-10-19 13:07:05 -0300

    initial commit

---
+++ MEModularApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MEModularApp
+//
+//  Created by Maria Eugênia Pereira Teixeira on 19/10/19.
+//  Copyright © 2019 Maria Eugênia Pereira Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-MEModularApp/Pods-MEModularApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MEModularApp : NSObject
+@end
+@implementation PodsDummy_Pods_MEModularApp
+@end
